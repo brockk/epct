a46ee7184c0fd5fa64bcba93f5e8b3ee84309133
Kristian Brock <kristian.brock@gmail.com>
2020-01-14 15:46:05 +0000

    Added day 2 materials

---
+++ InstallPrerequisites.R
@@ -0,0 +1,24 @@
+
+if(!require(bcrm)) {
+  install.packages('bcrm')
+}
+
+if(!require(dfcrm)) {
+  install.packages('dfcrm')
+}
+
+if(!require(trialr)) {
+  install.packages('trialr')
+}
+
+if(!require(dplyr)) {
+  install.packages('dplyr')
+}
+
+if(!require(ggplot2)) {
+  install.packages('ggplot2')
+}
+
+if(!require(devtools)) {
+  install.packages('devtools')
+}
